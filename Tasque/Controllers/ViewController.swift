9c69c53499e4a01d4fbb78c3735433e124474dd4
Ahmad Yasser <ahmadyas_ser@icloud.com>
2022-01-10 00:15:39 +0200

    add item class

---
+++ Tasque/Controllers/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  Tasque
+//
+//  Created by Ahmad Yasser on 08/01/2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    let defaults = UserDefaults.standard
+    
+    var itemArray = [Item]()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        let newItem = Item(title: "kill yourself", done: false)
+        itemArray.append(newItem)
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
+            itemArray = items
+        }
+    }
+        //MARK: - Add new item
+        @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+            var textField = UITextField()
+            
+            let alert = UIAlertController(title: "Add new Tasque item", message: "", preferredStyle: .alert)
+            
+            let action = UIAlertAction(title: "Add item", style: .default) { action in
+                //things that will happen when we click add item button
+                let newItem = Item(title: textField.text!, done: false)
+                
+                self.itemArray.insert(newItem, at: 0)
+                
+                self.tableView.reloadData()
+                
+                
+            }
+            alert.addTextField { alertTextField in
+                alertTextField.placeholder = "enter new item"
+                textField = alertTextField
+            }
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+        }
+        
+        //MARK: - TableView DataSource
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return itemArray.count
+        }
+        
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "tasqueCell", for: indexPath)
+            let item = itemArray[indexPath.row]
+            
+            cell.textLabel?.text = item.title
+            
+            cell.accessoryType = item.done ? .checkmark : .none
+            return cell
+        }
+        
+        //MARK: - TableView Delegate
+        override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            itemArray[indexPath.row].done.toggle()
+            tableView.deselectRow(at: indexPath, animated: true)
+            tableView.reloadData()
+        }
+    }
+
---
+++ Tasque/Data Model/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Tasque
+//
+//  Created by Ahmad Yasser on 09/01/2022.
+//
+
+import Foundation
+
+class Item {
+    var title: String
+    var done: Bool
+    
+    init(title: String, done: Bool) {
+        self.title = title
+        self.done = done
+    }
+}
