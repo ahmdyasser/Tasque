fa0de1f9559b3d5b118b3849186a4c14b75d1516
Ahmad Yasser <ahmadyas_ser@icloud.com>
2022-01-10 08:09:18 +0200

    Change app main theme

--- Tasque/Controllers/ViewController.swift
+++ Tasque/Controllers/ViewController.swift
@@ -18,7 +18,6 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         let newItem = Item(title: "kill yourself", done: false)
         itemArray.append(newItem)
         
@@ -26,51 +25,60 @@ class ViewController: UITableViewController {
             itemArray = items
         }
     }
-        //MARK: - Add new item
-        @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-            var textField = UITextField()
-            
-            let alert = UIAlertController(title: "Add new Tasque item", message: "", preferredStyle: .alert)
-            
-            let action = UIAlertAction(title: "Add item", style: .default) { action in
-                //things that will happen when we click add item button
-                let newItem = Item(title: textField.text!, done: false)
-                
-                self.itemArray.insert(newItem, at: 0)
-                
-                self.tableView.reloadData()
-                
-                
-            }
-            alert.addTextField { alertTextField in
-                alertTextField.placeholder = "enter new item"
-                textField = alertTextField
-            }
-            alert.addAction(action)
-            present(alert, animated: true, completion: nil)
-        }
+    //MARK: - Add new item
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
         
-        //MARK: - TableView DataSource
-        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return itemArray.count
-        }
+        let alert = UIAlertController(title: "Add new Tasque item", message: "", preferredStyle: .alert)
         
-        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let action = UIAlertAction(title: "Add item", style: .default) { action in
+            //things that will happen when we click add item button
+            let newItem = Item(title: textField.text!, done: false)
             
-            let cell = tableView.dequeueReusableCell(withIdentifier: "tasqueCell", for: indexPath)
-            let item = itemArray[indexPath.row]
+            self.itemArray.insert(newItem, at: 0)
             
-            cell.textLabel?.text = item.title
+            self.tableView.reloadData()
             
-            cell.accessoryType = item.done ? .checkmark : .none
-            return cell
+            
         }
-        
-        //MARK: - TableView Delegate
-        override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            itemArray[indexPath.row].done.toggle()
-            tableView.deselectRow(at: indexPath, animated: true)
-            tableView.reloadData()
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "enter new item"
+            textField = alertTextField
         }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
+    
+    //MARK: - TableView DataSource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tasqueCell", for: indexPath)
+        let item = itemArray[indexPath.row]
+        
+        let check = UIImageView(image: UIImage(systemName: "checkmark"))
+        check.tintColor = UIColor(named: K.Colors.blue)
+        cell.backgroundColor = UIColor(named: K.Colors.cell)
+        cell.textLabel?.textColor = UIColor(named: K.Colors.text)
+        tableView.backgroundColor = UIColor(named: K.Colors.background)
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryView = item.done ? check : .none
+        return cell
+    }
+    
+    //MARK: - TableView Delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        itemArray[indexPath.row].done.toggle()
+        tableView.deselectRow(at: indexPath, animated: true)
+        tableView.reloadData()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        navigationController?.navigationBar.barStyle = .black
+    }
+}
 
--- Tasque/Data Model/K.swift
+++ Tasque/Data Model/K.swift
@@ -9,4 +9,13 @@ import Foundation
 
 struct K {
     static var cellId = "tasqueCell"
+    struct Colors {
+        
+        static var background = "backgroundColor"
+        static var blue = "blueColor"
+        static var cell = "cellColor"
+        static var orangeColor = "backgroundColor"
+        static var lightCell = "lighterCellColor"
+        static var text = "textColor"
+    }
 }
